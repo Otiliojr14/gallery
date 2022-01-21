a47dd269c16bb4fdcee7ff9396a7710661df5c1a
Otilio <otiliojr1999@gmail.com>
2022-01-21 10:17:45 -0600

    First commit

---
+++ admin/functions/classUser.php
@@ -0,0 +1,62 @@
+<?php
+
+class User
+{
+
+    public $id_user;
+    public $user_name;
+    public $user_password;
+    public $first_name;
+    public $last_name;
+
+    public static function find_all_users()
+    {
+        return self::query_consult("SELECT * FROM users");
+    }
+
+    public static function find_user_by_id($user_id)
+    {
+        $result_user = self::query_consult("SELECT * FROM users WHERE id_user = {$user_id} LIMIT 1");
+
+
+        foreach ($result_user as $user) {
+            return $user;
+        }
+    }
+
+    public static function query_consult($sql)
+    {
+        global $conn;
+        $object_array = array();
+
+        $result_set = $conn->query_consult($sql);
+
+        foreach ($result_set as $row) {
+
+            // Crea un objeto para cada registro
+            $object_array[] = self::instatiation($row);
+        }
+
+        return $object_array;
+    }
+
+    public static function instatiation($row)
+    {
+        $object = new self;
+
+        foreach ($row as $attribute => $value) {
+            if ($object->has_the_attribute($attribute)) {
+                $object->$attribute = $value;
+            }
+        }
+
+        return $object;
+    }
+
+    private function has_the_attribute($attribute)
+    {
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($attribute, $object_properties);
+    }
+}
---
+++ admin/includes/admin_content.php
@@ -0,0 +1,37 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Admin
+                <small>Subheading</small>
+            </h1>
+
+            <?php
+
+            // $users = User::find_all_users();
+
+            // var_dump($users);
+
+            // foreach ($users as $user) {
+            //     echo $user->user_name;
+            // }
+
+            // $user = User::find_user_by_id(1);
+
+            // echo ($user->user_name);
+            ?>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
